8d7b36b4da168dbbf64d3b14575f57d7ee7bd4bd
최하은 <haeun4345@gmail.com>
2025-04-07 10:59:59 +0900

    chore[#1]: 프로젝트 초기 세팅

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-2xl">TEST</h1>
+      </header>
+    </div>
+  );
+}
