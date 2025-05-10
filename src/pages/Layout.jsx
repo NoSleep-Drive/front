7f58a2b0442ea06fa703e8adba55cb8cd968dda3
Subin <rla0010101@gmail.com>
2025-05-10 16:56:08 +0900

    feat[#3]: Layout 조건부 렌더링

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Layout() {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === '/signup' || location.pathname === '/onboarding';
+
   return (
     <div className="flex min-h-screen flex-col">
-      <Header companyName="ㅇㅇㅇ 업체"></Header>
+      {!isAuthPage && <Header companyName="ㅇㅇㅇ 업체" />}
 
       <main className="flex-1 bg-gray-100 p-6">
         <Outlet />
