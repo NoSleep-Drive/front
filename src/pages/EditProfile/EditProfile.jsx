b94395e56aa3e0808c3d9c5e1ec0b4f293321237
Subin <rla0010101@gmail.com>
2025-05-15 01:15:01 +0900

    feat[#10]: 수정 및 탈퇴 페이지 랜더링해서 상세 정보 요청 시 UI 개선

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -11,19 +11,24 @@ export default function EditProfile() {
     handleSubmit,
     handleDelete,
   } = useEditProfile();
-
   return (
     <section className="flex min-h-screen items-center justify-center bg-white p-4">
       <div className="w-full max-w-lg p-8">
         <h2 className="text-center text-2xl font-semibold">회원 정보 수정</h2>
-        <EditProfileForm
-          state={state}
-          isLoading={isLoading}
-          isDeleting={isDeleting}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          handleDelete={handleDelete}
-        />
+        {isLoading ? (
+          <div className="mt-4 flex justify-center">
+            <div className="h-8 w-8 animate-spin rounded-full border-b-2 border-blue-600"></div>
+          </div>
+        ) : (
+          <EditProfileForm
+            state={state}
+            isLoading={isLoading}
+            isDeleting={isDeleting}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            handleDelete={handleDelete}
+          />
+        )}
       </div>
     </section>
   );
